5dad18aab5ec653038cbcc94e92a207578fbb80a
Martin Dj <96339040+martin-djukanovic@users.noreply.github.com>
2022-05-09 12:18:42 +0200

    Create special_coverings.m

---
+++ (3,3)/gluing-curves/special_coverings.m
@@ -0,0 +1,15 @@
+/*  The condition (j1 - 432)^3 = 729*j1*j2 which is satisfied when C -> E1 is generic and C -> E2 is special, corresponds to the following
+    conditions on a and b:
+    a^3*b + w*a^3 + (3*w + 3)*a*b + 6*a - 2*b - 2*w,
+    a^3*b + w*a^3 - 3*w*a*b + (-6*w - 6)*a - 2*b - 2*w,
+    a^3*b + w*a^3 - 3*a*b + 6*w*a - 2*b - 2*w,
+    a^3*b + (-w - 1)*a^3 + (3*w + 3)*a*b + 6*w*a - 2*b + 2*w + 2,
+    a^3*b + (-w - 1)*a^3 - 3*w*a*b + 6*a - 2*b + 2*w + 2,
+    a^3*b + (-w - 1)*a^3 - 3*a*b + (-6*w - 6)*a - 2*b + 2*w + 2,
+    a^3*b + a^3 + (3*w + 3)*a*b + (-6*w - 6)*a - 2*b - 2,
+    a^3*b + a^3 - 3*w*a*b + 6*w*a - 2*b - 2,
+    a^3*b + a^3 - 3*a*b + 6*a - 2*b - 2,
+    a^3 + 3*w*a*b - 2,
+    a^3 + (-3*w - 3)*a*b - 2,
+    a^3 + 3*a*b - 2
+*/
