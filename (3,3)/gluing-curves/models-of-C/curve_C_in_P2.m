663d2b39238ec51eb5af7bf2bc73bc856db54d38
Martin Dj <96339040+martin-djukanovic@users.noreply.github.com>
2022-05-17 16:06:33 +0200

    Update curve_C_in_P2.m

--- (3,3)/gluing-curves/models-of-C/curve_C_in_P2.m
+++ (3,3)/gluing-curves/models-of-C/curve_C_in_P2.m
@@ -50,6 +50,7 @@ function MyCurve(a,b)
     return HyperellipticCurve((x^3 - 6*w*(a^2 - w)*x^2 + 3*w*(4*a^3 - 9*w^2*a^2 + 4)*x - 6*w*(a^2 + 2*a*w^2 - 2*w)*(a + 1)*(a + w))*(x^3 - 6*a*(a*w - 2)*x^2 - 3*w*(4*a^3 - 3*a^2*w^2 - 6*a*w + 1)*x - 6*w*(a + 1)*(a + w)*(a^2- 4*a*w^2 + w)));
   elif a*w^2 + b*w eq 1 then
     return HyperellipticCurve((x^3 - 6*w*(b^2 - w)*x^2 + 3*w*(4*b^3 - 9*w^2*b^2 + 4)*x - 6*w*(b^2 + 2*b*w^2 - 2*w)*(b + 1)*(b + w))*(x^3 - 6*b*(b*w - 2)*x^2 - 3*w*(4*b^3 - 3*b^2*w^2 - 6*b*w + 1)*x - 6*w*(b + 1)*(b + w)*(b^2- 4*b*w^2 + w)));
+    // or replace w by w^2 in the previous one, which gives an isomorphic curve via (x,y) --> (w^2*x,y)
   else
     return HyperellipticCurve(1/3/(2 + a^3 - 3*a*b + 3*a^2*b^2 + b^3)*( 
 		(2 + a^3 - 3*a*b + 3*a^2*b^2 + b^3)^2*x^3
@@ -64,4 +65,8 @@ function MyCurve(a,b)
 	  ));
   end if;
 end function;
+
+// examples:
 MyCurve(0,0);
+MyCurve(2,2);
+MyCurve(1,2);
