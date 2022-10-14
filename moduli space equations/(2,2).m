f3a6b918d66f9ce4d382fe9bc10976e1b1ed98f7
Martin Dj <96339040+martin-djukanovic@users.noreply.github.com>
2022-10-14 13:34:52 +0200

    Delete moduli space equations directory

--- moduli space equations/(2,2).m
+++
@@ -1,8 +0,0 @@
-/* The equation satisfied by the Igusa-Clebsch invariants [I2:I4:I6:10] for genus-2 curves C with a (2,2)-split Jacobian. It defines a surface in IP(1,2,3,5) */
-
-   2^11*3^9*5^5*I10^3
- + 2^2*3^7*I10^2*(27*I2^5 + 2200*I2^3*I4 - 12000*I2^2*I6 - 58000*I2*I4^2 + 240000*I4*I6)
- + 2^2*3^4*I10*(-3*I2^6*I4^2 + 18*I2^5*I4*I6 - 239*I2^4*I4^3 - 27*I2^4*I6^2 + 2688*I2^3*I4^2*I6
- 	+ 1828*I2^2*I4^4 - 9540*I2^2*I4*I6^2 - 14640*I2*I4^3*I6 + 10800*I2*I6^3 - 128*I4^5 + 28800*I4^2*I6^2)
- + (I2^2*I4^2 - 6*I2*I4*I6 - I4^3 + 9*I6^2)^2*(I2^3 + 80*I2*I4 - 384*I6)
-     
