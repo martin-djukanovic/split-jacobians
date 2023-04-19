e75a20d1bfef10b54a48a04bbfb347946ffd4c74
Martin Dj <96339040+martin-djukanovic@users.noreply.github.com>
2023-04-19 22:56:57 +0000

    Update complementary_curve.m

--- (2,2)/complementary_curve.m
+++ (2,2)/complementary_curve.m
@@ -9,9 +9,9 @@ E2:=Curve(P2, -y^2*z + d*x^3 + c*x^2*z + b*x*z^2 + a*z^3);
 E2:=EllipticCurve(E2,E2![0,1,0]);
 J:=SegreProduct([C,C]);
 J:=map<AmbientSpace(J)->P8|[X]>(J);                                  // X = [xu : xv : xw : yu : yv : yw : zu : zv : zw]
-KC := Scheme (P8, [X[2] + X[4], X[3] - X[7], X[6] + X[8]]) meet J;  // corresponds to the canonical class {(x,y),(x,-y)}
+KC := Scheme (P8, [X[2] + X[4], X[3] - X[7], X[6] + X[8]]) meet J;  // corresponds to the canonical class {(x,y),(x,-y)}; this is blown down
 CC := Scheme (P8, [X[2] - X[4], X[3] + X[7], X[6] + X[8]]) meet J; // corresponds to Jac(C)/E1=Ker(f1_*)= {(x,y),(-x,-y)}; under f1 it gives (x^2,y)+(x^2,-y)=O on E1.
-P3<[U]>:=ProjectiveSpace(K,3);
+P3:=ProjectiveSpace(K,3);
 g:=map<P8->P3|[X[1], X[5], X[2] + X[4], X[9]]>;    // this sends (x,y,u,v) to (u*x, v*y, v*x + u*y) = (-x^2, -y^2, -2*x*y), so that (x,y,u,v) and (u,v,x,y) have the same image
 EE:=g(CC);                                        // the quotient of CC under the S2 action is the complementary curve
 E:=EllipticCurve(Curve(EE),EE![0,-d,0,1]);       // EE![0,1,0,0] is singular and requires sqrt(a) to resolve
