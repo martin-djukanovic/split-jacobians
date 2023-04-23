a3b2061bf6c43d5059251ba8de8faaa10fb1bda2
Martin Dj <96339040+martin-djukanovic@users.noreply.github.com>
2023-04-23 14:09:27 +0000

    Update defining_equations.m

--- (3,3)/gluing-curves/defining_equations.m
+++ (3,3)/gluing-curves/defining_equations.m
@@ -1,7 +1,7 @@
 /*
   Fix two elliptic curves in the Hesse pencil, with the identity element [-1:1:0];
-  E1: x^3 + y^3 + z^3 + s*x*y*z = 0
-  E2: x^3 + y^3 + z^3 + t*x*y*z = 0
+  E1: x^3 + y^3 + z^3 + 3*a*x*y*z = 0
+  E2: x^3 + y^3 + z^3 + 3*b*x*y*z = 0
   From the formulas in the Appendix, we have the following on E1 and E2:
   - the automorphism [-1] is given by [x : y : z] |--> [y : x : z]
   - translation by  S=[-1 : 0 : 1] is given by   [x : y : z] |--> [y : z : x]
@@ -12,17 +12,19 @@
 
 R<x> := PolynomialRing(QQ);
 K<w> := NumberField(1+x+x^2);
-L<s,t> := FunctionField(K,2);
+L<a,b> := FunctionField(K,2);
+s:=3*a;
+t:=3*b;
 P8<X1,X2,X3,X4,X5,X6,X7,X8,X9> := ProjectiveSpace(L,8);
 
 // equations defining the surface A=SegreProduct([E1,E2])
 A := Scheme(P8, [
-  X3^3 + X6^3 + X9^3 + s*X3*X6*X9,
-  X2^3 + X5^3 + X8^3 + s*X2*X5*X8,
-  X1^3 + X4^3 + X7^3 + s*X1*X4*X7,
-  X7^3 + X8^3 + X9^3 + t*X7*X8*X9,
-  X4^3 + X5^3 + X6^3 + t*X4*X5*X6,
-  X1^3 + X2^3 + X3^3 + t*X1*X2*X3,
+  X3^3 + X6^3 + X9^3 + 3*a*X3*X6*X9,
+  X2^3 + X5^3 + X8^3 + 3*a*X2*X5*X8,
+  X1^3 + X4^3 + X7^3 + 3*a*X1*X4*X7,
+  X7^3 + X8^3 + X9^3 + 3*b*X7*X8*X9,
+  X4^3 + X5^3 + X6^3 + 3*b*X4*X5*X6,
+  X1^3 + X2^3 + X3^3 + 3*b*X1*X2*X3,
   X6*X8 - X5*X9,
   X3*X8 - X2*X9,
   X6*X7 - X4*X9,
@@ -53,27 +55,27 @@ Hence on A we have:
 - A[2] consists of the points fixed by [-1] so it is the union of the schemes
   S1 := Scheme(P8, [X1-X5, X2-X4, X3-X6, X7-X8]) meet A;
   S2 := Scheme(P8, [X1+X5, X2+X4, X3+X6, X7+X8, X9]) meet A;
-- over K(s,t), we find the irreducible components of A[2] to be A20, A21, A22, A23, defined below
+- over K(a,b), we find the irreducible components of A[2] to be A20, A21, A22, A23, defined below
 */
 
 // the identity point O on A
 A20 := Scheme(P8, [ X1 - X5, X2 + X5, X3, X4 + X5, X6, X7, X8, X9 ]);
 
 // the three points of order two on E1 (embedded in A)
-A21 := Scheme(P8, [ 2*X5^3 + s*X5^2*X8 + X8^3, X1 + X5, X2 - X5, X3, X4 + X5, X6, X7 + X8, X9 ]);
+A21 := Scheme(P8, [ 2*X5^3 + 3*a*X5^2*X8 + X8^3, X1 + X5, X2 - X5, X3, X4 + X5, X6, X7 + X8, X9 ]);
 
 // the three points of order two on E2 (embedded in A)
-A22 := Scheme(P8, [ 2*X5^3 + t*X5^2*X6 + X6^3, X1 + X5, X2 + X5, X3 + X6, X4 - X5, X7, X8, X9 ]);
+A22 := Scheme(P8, [ 2*X5^3 + 3*b*X5^2*X6 + X6^3, X1 + X5, X2 + X5, X3 + X6, X4 - X5, X7, X8, X9 ]);
 
 // the remaining nine points of A[2]
 A23 := Scheme(P8, [
-  4*X5^3 - s*t*X5^2*X9 - s*X6^2*X9 - t*X8^2*X9 - X9^3,
-  2*X5^2*X6 + s*X5^2*X9 + X8^2*X9,
-  2*X5*X6^2 + s*X5*X6*X9 + X8*X9^2,
-  2*X6^3 + s*X6^2*X9 + X9^3,
-  2*X5^2*X8 + t*X5^2*X9 + X6^2*X9,
-  2*X5*X8^2 + t*X5*X8*X9 + X6*X9^2,
-  2*X8^3 + t*X8^2*X9 + X9^3,
+  4*X5^3 - 9*a*b*X5^2*X9 - 3*a*X6^2*X9 - 3*b*X8^2*X9 - X9^3,
+  2*X5^2*X6 + 3*a*X5^2*X9 + X8^2*X9,
+  2*X5*X6^2 + 3*a*X5*X6*X9 + X8*X9^2,
+  2*X6^3 + 3*a*X6^2*X9 + X9^3,
+  2*X5^2*X8 + 3*b*X5^2*X9 + X6^2*X9,
+  2*X5*X8^2 + 3*b*X5*X8*X9 + X6*X9^2,
+  2*X8^3 + 3*b*X8^2*X9 + X9^3,
   X6*X8 - X5*X9,
   X1 - X5,
   X2 - X5,
@@ -82,14 +84,14 @@ A23 := Scheme(P8, [
   X7 - X8
 ]);
 
-// Divisors E1, E2, E1 + E2 and 3(E1+E2)
-E1 := Scheme(P8, [X1^3 + X4^3 + X7^3 + s*X1*X4*X7, X3, X6, X9, X1 + X2, X4 + X5, X7 + X8]);
-E2 := Scheme(P8, [X1^3 + X2^3 + X3^3 + t*X1*X2*X3, X7, X8, X9, X1 + X4, X2 + X5, X3 + X6]);
-Th := Scheme(P8, [X1+X2+X4+X5, X3+X6, X7+X8, X9]) meet A;
-Th3 := Scheme(P8,[X1 + X2 + X4 + X5 - a*(X3 + X6) - b*(X7 + X8) + a*b*X9]) meet A;
+// Divisors E1, E2, E1 + E2 and 3(E1 + E2)
+E1 := Scheme(P8, [X1^3 + X4^3 + X7^3 + 3*a*X1*X4*X7, X3, X6, X9, X1 + X2, X4 + X5, X7 + X8]);
+E2 := Scheme(P8, [X1^3 + X2^3 + X3^3 + 3*b*X1*X2*X3, X7, X8, X9, X1 + X4, X2 + X5, X3 + X6]);
+Theta := Scheme(P8, [X1 + X2 + X4 + X5, X3 + X6, X7 + X8, X9]) meet A;
+Theta3 := Scheme(P8, X1 + X2 + X4 + X5 - b*(X3 + X6) - a*(X7 + X8) + a*b*X9) meet A; // since 3*O on E1 is defined by x + y - a*z = 0
 
 // the hyperplane section on A invariant under the actions of G and [-1]
-H1 := Scheme(P8, [ X1 + X5 + X9 ]);  // or X2 + X4 + X9 if G is the graph of the same isomorphism composed with [-1]
+H1 := Scheme(P8, X1 + X5 + X9);  // or X2 + X4 + X9 if G is the graph of the same isomorphism composed with [-1]
 D := H1 meet A;
 
 // D does not contain O, but it contains the order-2 points of E1 and E2 (embedded in A)
